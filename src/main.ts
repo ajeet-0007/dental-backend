03a1cb15344696238f0902d589176c73309d49ab
Apple <apple@Apples-MacBook-Air.local>
2026-08-16 14:09:53 +0530

    ini

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,7 @@ import { ValidationPipe } from "@nestjs/common";
 import { SwaggerModule, DocumentBuilder } from "@nestjs/swagger";
 import { NestExpressApplication } from "@nestjs/platform-express";
 import * as express from "express";
-import * as cookieParser from "cookie-parser";
+import cookieParser from "cookie-parser";
 import { Request, Response, NextFunction } from "express";
 import * as uuid from "uuid";
 import { AppModule } from "./app.module";
